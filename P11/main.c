9ad1c336bb08bdcd0bb3c1931167de3f0c45aa7a
alexander261 <kevin.pancho@uabc.edu.mx>
2025-11-18 19:49:06 +0000

    p11

--- P11/main.c
+++ P11/main.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
-
+#include <stdio.h>
 
 extern void pBin8b (uint8_t dato);
+
 
 /*
 extern void pBin16b (uint16_t dato );
@@ -12,7 +13,10 @@ extern void pBin64b (uint64_t dato );
 int main(){
 
 
-    pBin8b(10);
+    printf("INICIO :   \n");
+    pBin8b(8);
+    printf("\n");
+    printf("FIN :   \n");
 
     return 0;
 }
--- P11/p11.asm
+++ P11/p11.asm
@@ -1,6 +1,7 @@
-%include "../LIB/pc_iox.inc"
 ;gcc -m32 main.c p11.o ../LIB/libpc_iox.a -o main
 ;nasm -f elf p11.asm
+
+extern putchar
 
 section .text
     global pBin8b
@@ -8,20 +9,9 @@ pBin8b:
     push ebp
     mov ebp,esp
 
-    mov eax,10 ;Valor prueba
-    mov ecx,32 ;iteraciones prueba
+    mov eax,[ebp + 8]
+    mov ecx,8
     mov esi,0
-
-
-    push ecx
-    
-    mov ebx,32
-    sub ebx,ecx ;Diferencia
-    
-    mov ecx,ebx
-    shl eax,cl
-    
-    pop ecx
 
     IMPRIMIR_BIT:
         push ecx
@@ -31,7 +21,7 @@ pBin8b:
         mov ecx,esi
         
         
-        shl eax,cl
+        shl al,cl
             JC IMPRIMIR_UNO
         
         mov al,'0'
@@ -41,12 +31,28 @@ pBin8b:
             mov al,'1'
         
     CONTINUAR_IMPRIMIR_BIT:
-        call putchar
+        call myPutchar
         
         pop eax
         pop ecx
 
         LOOP IMPRIMIR_BIT
 
+
     pop ebp
     ret
+
+myPutchar:
+    push eax
+    push ecx
+    push edx
+
+    push eax
+    call putchar
+    pop eax
+
+    pop edx
+    pop ecx
+    pop eax
+    ret
+
